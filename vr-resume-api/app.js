97b4268d655fdf93d6989cc8b67cb3bda3ee05a1
Jesus Merlos <jmerlosrepos@outlook.com>
2023-01-17 17:08:48 -0500

    coirs and upload bug solved

--- vr-resume-api/app.js
+++ vr-resume-api/app.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const cors = require("cors");
-//const upload = require("express-fileupload");
-// const career = require("./routes/career");
-// const education = require("./routes/education");
-// const login = require("./routes/login");
-// const profile = require("./routes/profile");
-// const projects = require("./routes/projects");
-// const register = require("./routes/register");
-// const resume = require("./routes/resume");
-// const sections = require("./routes/sections");
-// const selfPresentation = require("./routes/selfPresentation");
+const upload = require("express-fileupload");
+const career = require("./routes/career");
+const education = require("./routes/education");
+const login = require("./routes/login");
+const profile = require("./routes/profile");
+const projects = require("./routes/projects");
+const register = require("./routes/register");
+const resume = require("./routes/resume");
+const sections = require("./routes/sections");
+const selfPresentation = require("./routes/selfPresentation");
 
 const app = express();
 
 const PORT = process.env.PORT | 3001;
 
-app.use(cors);
-//app.use(upload);
-// app.use("/career", career);
-// app.use("/education", education);
-// app.use("/login", login);
-// app.use("/profile", profile);
-// app.use("/projects", projects);
-// app.use("/register", register);
-// app.use("/resume", resume);
-// app.use("/sections", sections);
-// app.use("/selfPresentation", selfPresentation);
+app.use(cors());
+app.use(upload());
+app.use("/career", career);
+app.use("/education", education);
+app.use("/login", login);
+app.use("/profile", profile);
+app.use("/projects", projects);
+app.use("/register", register);
+app.use("/resume", resume);
+app.use("/sections", sections);
+app.use("/selfPresentation", selfPresentation);
 
 app.get("/", (req, res) => {
-  res.send("OK");
+  res.send("OK Port");
 });
 
 app.listen(PORT, () => {
